19f01b455a1e71114726221040c64ff7c19d9c26
elvirusgamer <elvirusgamer2.0@gmail.com>
2024-08-07 11:25:55 -0500

    creacion de formulario dinamico

---
+++ src/app/components/dynamic-form-question/dynamic-form-question.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-form-question',
+  standalone: true,
+  imports: [],
+  templateUrl: './dynamic-form-question.component.html',
+  styleUrl: './dynamic-form-question.component.scss'
+})
+export class DynamicFormQuestionComponent {
+
+}
